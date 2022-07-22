d22de2d1c87a5376710c93417432c523c377a997
Daniel Stekol <daniel.stekol@gmail.com>
2022-07-22 14:07:12 -0400

    random player

--- src/players/RandomPlayer.py
+++ src/players/RandomPlayer.py
@@ -5,15 +5,19 @@ class RandomPlayer(ConnectFourPlayer):
   gaussian_col_weights = [0.3, 0.6, 0.8, 1, 0.8, 0.6, 0.3]
                           
   def __init__(self, gaussian=True):
-    super(RandomPlayer, self).__init__(game)
+    super(RandomPlayer, self).__init__()
     self.gaussian = gaussian
 
   def pick_move(self, game):
-    valid_cols = [col for col in range(game.cols) if game.level[col] >= 0]
+    valid_cols = game.valid_moves()
     if (self.gaussian):
       return self.pick_random_gaussian(valid_cols)
     else:
-      return random.choice(valid_cols)
+      return self.pick_random_uniform(valid_cols)
+
+  @classmethod
+  def pick_random_uniform(cls, cols):
+    return random.choice(valid_cols)
 
   @classmethod
   def pick_random_gaussian(cls, cols):
