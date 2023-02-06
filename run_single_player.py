321a5f0b8b57f41ef8a329ed3f1e697654facd0e
Daniel Stekol <daniel.stekol@gmail.com>
2023-02-05 20:24:15 -0500

    updated interactive scripts

--- run_single_player.py
+++ run_single_player.py
@@ -1,85 +1,73 @@
 from src.ConnectFour import ConnectFour
 from src.players.AlphaBetaPlayer import AlphaBetaPlayer
 from src.players.MCTSPlayer import MCTSPlayer
-from src.players.AlphaZeroPlayer import AlphaZeroPlayer #, eval_func
-from src.players.components.EvalBuffer import EvalBuffer
-from src.players.components.AlphaZeroNets import AlphaZeroFCN, AlphaZeroCNN
+from src.players.AlphaZeroPlayer import AlphaZeroPlayer
+from src.players.alphazero.BufferedModelWrapper import BufferedModelWrapper
+from src.players.alphazero.AlphaZeroNets import AlphaZeroCNN
+from parse_utils import parse_args_single_player
+from validation_utils import AgentType
 import timeit
 import numpy as np
 from torch import zeros
+import random
 
-np.set_printoptions(precision=3)
 
-cnct4 = ConnectFour()
-#player = AlphaBetaPlayer(max_depth=8)
-
-mcts_args = {
-            "discount": 0.96,
-            "mcts_iters": 4000,
-            "explore_coeff": 1,
-            "temperature": 0,
-            "dirichlet_coeff": 0,
-            "dirichlet_alpha": 0.03,
-            "num_threads": 30
-            }
-
-class FakeModel:
-  def __init__(self):
-    self.device = "cpu"
-
-  def __call__(self, inp, apply_softmax=False):
-    a = zeros(inp.shape[0], 7, device=self.device)
-    a[:] = 1 / 7
-    return a, zeros(inp.shape[0], 1, device=self.device)
-
-model = FakeModel()
-
-#player = MCTSPlayer(mcts_args)
-#model = AlphaZeroFCN.load_from_checkpoint("sl_model.ckpt").cuda()
-model = AlphaZeroCNN(0, 0, 0)
-buffer = EvalBuffer(model, 15, 2)
-player = AlphaZeroPlayer(buffer, mcts_args)
-is_over = False
-winner = 0
-human_first = True
-def print_eval(game):
-  actions = game.valid_moves()
-  #action_vals, state_vals = eval_func(model, game, actions)
-  #print(action_vals)
-  #print(state_vals)
-if (not human_first):
-  #cnct4.perform_move(player.pick_move(cnct4))
-  #cnct4.perform_move(3)
-  t = timeit.timeit(lambda: cnct4.perform_move(player.pick_move(cnct4)), number=1)
-  print("time: " + str(t))
-cnct4.print_board()
-while(not is_over):
-  print(len(player.mcts.nodes))
-  #player.mcts.print_entropy()
-  try: 
-    print_eval(cnct4)
-    col = int(input(f"Move: ")) - 1
-    cnct4.perform_move(col, validate=True)
+def play_single_player(agent, human_first):
+  cnct4 = ConnectFour()
+  is_over = False
+  winner = 0
+  if (not human_first):
+    cnct4.perform_move(agent.pick_move(cnct4))
+  cnct4.print_board()
+  while(not is_over):
+    try: 
+      col = int(input(f"Move: ")) - 1
+      cnct4.perform_move(col, validate=True)
+      is_over, winner = cnct4.game_over()
+      cnct4.print_board()
+      if (is_over):
+        break
+      cnct4.perform_move(agent.pick_move(cnct4))
+      cnct4.print_board()
+    except ValueError as e:
+      print(e)
+      continue
     is_over, winner = cnct4.game_over()
-    cnct4.print_board()
-    if (is_over):
-      break
-    #print_eval(cnct4)
-    #move = player.pick_move(cnct4)
-    #print(player.mcts.current_action_scores(cnct4, temperature=1))
-    #cnct4.perform_move(move)
+  results = {1: "Player 1 Wins", 0: "Tie", -1: "Player 2 Wins"}
+  print("Game Over: " + results[winner])
 
-    t = timeit.timeit(lambda: cnct4.perform_move(player.pick_move(cnct4)), number=1)
-    print("time: " + str(t))
-    cnct4.print_board()
-  except ValueError as e:
-    print(e)
-    continue
-  is_over, winner = cnct4.game_over()
-results = {1: "Player 1 Wins", 0: "Tie", -1: "Player 2 Wins"}
-print("Game Over: " + results[winner])
+def initialize_agent(args):
+  agent_type = AgentType(args.pop("agent"))
 
-buffer.close()
+  if (agent_type == AgentType.RANDOM):
+    return RandomPlayer(**args)
+  elif (agent_type == AgentType.ALPHABETA):
+    return AlphaBetaPlayer(**args)
+  elif (agent_type == AgentType.MCTS):
+    gaussian = args.pop("gaussian")
+    return MCTSPlayer(args, gaussian)
+  elif (agent_type == AgentType.ALPHAZERO):
+    use_cuda = args.pop("cuda")
+    checkpoint_path = args.pop("checkpoint")
+    max_buffer_size = args.pop("max_buffer_size")
+    max_wait_time = args.pop("max_wait_time")
 
-#t = timeit.timeit(lambda: player.pick_move(cnct4), number = 1)
-#print(t)
+    device = "cuda" if use_cuda else "cpu"
+    model = AlphaZeroCNN.load_from_checkpoint(checkpoint_path).to(device)
+    buffered_model = BufferedModelWrapper(model, max_buffer_size, max_wait_time)
+    return AlphaZeroPlayer(buffered_model, args)
+  raise ValueError(f"Cannot load agent of type {agent_type}")
+
+
+if __name__ == "__main__":
+  args = parse_args_single_player()
+  
+  human_first = args.pop("human_first")
+  if (human_first is None):
+    human_first = bool(random.getrandbits(1))
+  agent = initialize_agent(args)
+
+  play_single_player(agent, human_first)
+
+  if (isinstance(agent, AlphaZeroPlayer)):
+    agent.close()
--- showdown.py
+++ showdown.py
@@ -1,111 +1,32 @@
-from src.players.MCTSPlayer import MCTSPlayer
 from src.ConnectFour import ConnectFour
 from src.players.AlphaZeroPlayer import AlphaZeroPlayer
-from src.players.components.AlphaZeroNets import AlphaZeroCNN
-from src.players.components.EvalBuffer import EvalBuffer
-import random
-import pickle as pkl
-from tqdm import tqdm
+from run_single_player import initialize_agent
+from parse_utils import parse_args_showdown
 
-#random.seed(2)
-
-trajectories = []
-#filename="2000game_data.pkl"
-temp = 0.5
-mcts_iters = 500
-#log_freq = 10
-pbar = tqdm()
-i = 0
-win_counter = 0
-first_winner_counter = 0
-
-subopt_counter = 0
-subopt_summer = 0
-subopt_total = 0
-
-mcts_args = {
-            "discount": 0.96,
-            "mcts_iters": 250,
-            "explore_coeff": 1,
-            "temperature":  0.1,
-            "dirichlet_coeff": 0.02,
-            "dirichlet_alpha": 0.3,
-            "num_threads": 15
-            }
-
-model1 = AlphaZeroCNN(0, 0, 0).cuda()
-buffer1 = EvalBuffer(model1, 5, 0.5)
-model2 = AlphaZeroCNN.load_from_checkpoint("checkpoints_v5/0.ckpt").cuda()
-buffer2 = EvalBuffer(model2, 5, 0.5)
-num_moves = 0
-
-num_games = 100
-drop_move = 0
-
-for i in tqdm(range(num_games)):
-  #if (i > 0):
-  #  print("\nProportion")
-  #  print(float(win_counter) / float(i))
-  #player1 = MCTSPlayer(mcts_iters=mcts_iters, temperature=temp, discount=1)
-  #player2 = MCTSPlayer(mcts_iters=mcts_iters, temperature=temp, discount=0.96)
-  player1 = AlphaZeroPlayer(buffer1, mcts_args)
-  player2 = AlphaZeroPlayer(buffer2, mcts_args)
+def showdown(player1, player2):
   cnct4 = ConnectFour()
   is_over = False
-  moves = []
-  scores = []
-  if (bool(random.getrandbits(1))):
-    #print("small going first")
-    player1, player2 = player2, player1
-  else:
-    pass
-    #print("big going first")
+  playerA, playerB = player1, player2
   while (not is_over):
-    #if (cnct4.num_moves == drop_move):
-    #  print("dropping")
-    #  player1.drop_temperature()
-    #  player2.drop_temperature()
-    move = player1.pick_move(cnct4)
-    moves.append(move)
-    scores.append(player1.mcts.current_action_scores(cnct4))
-    
-    if (move != scores[-1].argmax()):
-      subopt_counter += 1
-      d = max(scores[-1]) - scores[-1][move]
-      subopt_summer += d
-      if (d >= 0.2):
-        cnct4.print_board()
-        print(scores[-1].round(4))
-        print(f"argmax: {scores[-1].argmax()}")
-        print(f"move: {move}")
-    subopt_total += 1
+    move = playerA.pick_move(cnct4)
     cnct4.perform_move(move)
     player1, player2 = player2, player1
-    #cnct4.print_board()
-    #print(scores[-1].round(4))
-    #print(f"argmax: {scores[-1].argmax()}")
+    cnct4.print_board()
     is_over, winner = cnct4.game_over()
-  num_moves += cnct4.num_moves
-  if (player2.buffer == buffer2): # player 1 won
-    #print("Win")
-    win_counter += 1
-  #else:
-  #  print("Lose")
+    if (not is_over):
+      input("Press Enter for next move")
   if (winner == 1):
-    first_winner_counter += 1
-  trajectories.append((moves, scores, winner))
-  pbar.update()
-  #if ( i % log_freq == 0):
-  #  pkl.dump(trajectories, open(filename, "wb"))
-  #i += 1
-#pkl.dump(trajectories, open(filename, "wb")
-print(f"\nNum wins: {win_counter}")
-print(f"\nNum first wins: {first_winner_counter}")
-print(f"\nAvg num moves: {num_moves / num_games}")
-print(f"drop move: {drop_move}")
-print(f"subopt moves: {subopt_counter / subopt_total}")
-print(f"temp: {mcts_args['temperature']}")
-print(f"noise: {mcts_args['dirichlet_coeff']}")
-print(f"subopt diff: {subopt_summer / subopt_counter}")
-buffer1.close()
-buffer2.close()
+    print("Player 1 Wins")
+  elif (winner == -1) :
+    print("Player 2 Wins")
+  else:
+    print("Tie")
+  
+if __name__ == "__main__":
+  args = parse_args_showdown()
+  player1 = initialize_agent(args["agent1args"])
+  player2 = initialize_agent(args["agent2args"])
+  showdown(player1, player2)
+  for player in [player1, player2]:
+    if (isinstance(player, AlphaZeroPlayer)):
+      player.close()
